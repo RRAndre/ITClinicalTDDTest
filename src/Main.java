e591aa813746a986193dc2aec7b6bb2052d6b6b2
André Martins <r.andre_sm@hotmail.com>
2022-09-11 17:52:08 +0100

    End

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         UppercaseLettersFilter uppercaseLettersFilter = new UppercaseLettersFilter();
         uppercaseLettersFilter.filterUppercase(2, "THIS IS A text STRING THAT i am uSInG to CHEck my Output");
-        uppercaseLettersFilter.filterUppercase(-1, "ITCLiNicAl");
+        uppercaseLettersFilter.filterUppercase(4, "ITCLiNicAl");
         uppercaseLettersFilter.filterUppercase(1, "ITCLiNicAl");
         uppercaseLettersFilter.filterUppercase(2, "ITCLiNicAl");
         uppercaseLettersFilter.filterUppercase(3, "ITCLiNicAl");
-        uppercaseLettersFilter.filterUppercase(100, "ITCLiNicAl");
-        uppercaseLettersFilter.filterUppercase(10, "ITCLiNicAl");
-        uppercaseLettersFilter.filterUppercase(0, "ITCLiNicAl");
+        uppercaseLettersFilter.filterUppercase(9, "ITCLiNicAl");
+        uppercaseLettersFilter.filterUppercase(8, "ITCLiNicAl");
+        uppercaseLettersFilter.filterUppercase(6, "ITCLiNicAl");
     }
 }
--- src/UppercaseLettersFilter.java
+++ src/UppercaseLettersFilter.java
@@ -2,49 +2,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UppercaseLettersFilter {
-        private int N = 1;
-        private String text = null;
+    private int N = 1;
+    private String text = null;
+    StringBuilder builder;
+    Map<Character, Integer> charCounterMap;
+
     public String filterUppercase(int N, String text) {
+
         this.N = N;
         this.text = text;
-
-        StringBuilder builder = new StringBuilder();
-        Map<Character, Integer> charCounterMap = new HashMap<>();
-
-        if (text.length() == 0) {
-            throw new IllegalArgumentException("Please provide a text to be filtered.");
+        builder = new StringBuilder();
+        charCounterMap = new HashMap<>();
+        checkInvalidConditions();
+        checkValidConditions();
+        String resultString = builder.toString();
+        System.out.println(resultString);
+        for (Map.Entry entry : charCounterMap.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
         }
-        if (N <= 0 || N >= text.length()) {
-            throw new IllegalArgumentException("N needs to be a positive integer, and smaller than " + text.length());
-        }
-        else {
-            char[] chars = text.toCharArray();
-            for (int i = N - 1; i < chars.length; i += N) {
-                char ch = chars[i];
-                if (!Character.isLowerCase(ch) && !Character.isWhitespace(ch)) {
-                    builder.append(ch);
-                    if(!charCounterMap.containsKey(ch)){
-                        charCounterMap.put(ch, 1);
-                    }
-                    else {
-                        charCounterMap.put(ch,charCounterMap.get(ch) + 1);
-                    }
-                }
-            }
-            String resultString = builder.toString();
-            System.out.println(resultString);
-            for (Map.Entry entry : charCounterMap.entrySet()) {
-                System.out.println(entry.getKey() + " = " + entry.getValue());
-            }
-            return resultString;
-        }
+        return resultString;
     }
-    public void checkInvalidCondition(){
-        if (text.length() == 0) {
-            throw new IllegalArgumentException("Please provide a text to be filtered.");
-        }
-        if (N <= 0 || N >= text.length()) {
-            throw new IllegalArgumentException("N needs to be a positive integer, and smaller than " + text.length());
+    public void checkInvalidConditions() {
+        if (text.length() == 0) throw new IllegalArgumentException("Please provide a text to be filtered.");
+        if (N <= 0 || N >= text.length()) throw new IllegalArgumentException("N needs to be a positive integer, and smaller than " + text.length());
+        else return;
+    }
+    public void checkValidConditions() {
+        char[] chars = text.toCharArray();
+        for (int i = N - 1; i < chars.length; i += N) {
+            char ch = chars[i];
+            if (!Character.isLowerCase(ch) && !Character.isWhitespace(ch)) {
+                builder.append(ch);
+                if (!charCounterMap.containsKey(ch)) charCounterMap.put(ch, 1);
+                else charCounterMap.put(ch, charCounterMap.get(ch) + 1);
+            }
         }
     }
 }
